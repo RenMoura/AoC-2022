3354cc6fe682ffa5c800b4db73a28970cc9f7e85
APAC\ole1yh <Renato.OliveiraMoura@jp.bosch.com>
2022-12-06 11:31:16 +0900

    day 1 - part 1

---
+++ Day1/day1.py
@@ -0,0 +1,17 @@
+"""Get elf carrying most number of calories"""
+
+current_elf = 0
+elfs = []
+with open("Day1/input.txt","r") as input_file:
+    elfs.append(0)
+    for line in input_file:
+        line = line.strip("\n")
+        if line == '':
+            current_elf = current_elf + 1
+            elfs.append(0)
+        else:
+            elfs[current_elf] = elfs[current_elf] + int(line)
+
+    print(max(elfs))
+
+
