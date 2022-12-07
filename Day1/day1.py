2241f0a441afaabdbc04a7e6bc5fc486fe4d20b6
APAC\ole1yh <Renato.OliveiraMoura@jp.bosch.com>
2022-12-07 20:44:42 +0900

    day1 part two

--- Day1/day1.py
+++ Day1/day1.py
@@ -11,7 +11,14 @@ with open("Day1/input.txt","r") as input_file:
             elfs.append(0)
         else:
             elfs[current_elf] = elfs[current_elf] + int(line)
+    max_elf_1 = max(elfs)
+    elfs.remove(max_elf_1)
+    max_elf_2 = max(elfs)
+    elfs.remove(max_elf_2)
+    max_elf_3 = max(elfs)
+    elfs.remove(max_elf_3)
 
-    print(max(elfs))
+    print(max_elf_1+max_elf_2+max_elf_3)
+
 
 
