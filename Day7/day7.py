8b241da291d3d0ef7a684a0d5d2616c0bf05e058
renmoura <renamoura19@gmail.com>
2022-12-11 21:28:06 +0900

    day 7

--- Day7/day7.py
+++ Day7/day7.py
@@ -22,15 +22,35 @@ class Directory:
         return total
 
 with open("Day7/input.txt","r") as command_line_history:
-    cur_dir = ''
-    parent_directory = Directory('/')
+    cur_dir = 'home'
+    directory_tree = { cur_dir: Directory('home')}
     for line in command_line_history:
         if line[0] == "$":
-            if re.search(r"\$\scd\s(.+)",line):
+            if re.search(r"\$\scd\s(\w+)",line):
+                #directory_tree[cur_dir].add_directory(cur_dir + "/" + re.search(r"\$\scd\s(.+)",line).group(1))
                 cur_dir = cur_dir + "/" + re.search(r"\$\scd\s(.+)",line).group(1)
-        elif re.search(r"(\d+)\s(\w+\.\w+)",line):
+                #directory_tree[cur_dir] = Directory(cur_dir)
+            elif re.search(r"\$\scd\s(\.\.)",line):
+                cur_dir = re.sub(r"(.+)\/\w+$",r"\1",cur_dir)
+        elif re.search(r"(\d+)\s(.+)",line):
+            match_file = re.search(r"(\d+)\s(.+)",line)
+            directory_tree[cur_dir].add_file(match_file.group(2),int(match_file.group(1)))
+        elif re.search(r"dir\s(.+)",line):
+            directory_tree[cur_dir + "/" + re.search(r"dir\s(.+)",line).group(1)] = Directory(cur_dir + "/" + re.search(r"dir\s(.+)",line).group(1))
+            directory_tree[cur_dir].add_directory(directory_tree[cur_dir + "/" + re.search(r"dir\s(.+)",line).group(1)])
+    size_sum = 0
+    total_space = 70000000
+    need_space = 30000000
+    free_space = total_space - directory_tree['home'].get_dir_size()
+    minimum_folder_size = total_space
 
-
-
-
-
+    for directory in directory_tree:
+        dir_size = directory_tree[directory].get_dir_size()
+        print(f"directory {directory} = {dir_size}")
+        if dir_size <= 100000:
+            size_sum += dir_size
+        if free_space+dir_size > need_space:
+            minimum_folder_size = min(dir_size,minimum_folder_size)    
+    
+    print(size_sum)
+    print(minimum_folder_size)
